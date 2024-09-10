42174ce117ec81a346d2c1cb8027fd0242a817ad
Arun Isaac <arunisaac@systemreboot.net>
2024-09-10 17:11:00 +0100

    dir-locals: Set fill-column to 80 only in scheme-mode.
    For other major modes, especially in magit commit message buffers, we
    want the default fill-column of 70.
    
    * .dir-locals.el (nil)[fill-column]: Move to scheme-mode.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,11 +2,11 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (fill-column . 80)
   (indent-tabs-mode))
  (makefile-gmake-mode
   (indent-tabs-mode t))
  (scheme-mode
+  (fill-column . 80)
   (eval . (put 'assoc-set 'scheme-indent-function 1))
   (eval . (put 'build-gexp-script 'scheme-indent-function 1))
   (eval . (put 'call-with-current-directory 'scheme-indent-function 1))
