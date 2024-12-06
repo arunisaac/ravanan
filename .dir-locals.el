3b4a5869337b968a235b31cad0e44776aa1a6717
Arun Isaac <arunisaac@systemreboot.net>
2024-12-06 23:42:07 +0000

    dir-locals: Indent call-with-inferior correctly.
    * .dir-locals.el (scheme-mode): Indent call-with-inferior correctly.

--- .dir-locals.el
+++ .dir-locals.el
@@ -10,5 +10,6 @@
   (eval . (put 'assoc-set 'scheme-indent-function 1))
   (eval . (put 'build-gexp-script 'scheme-indent-function 1))
   (eval . (put 'call-with-current-directory 'scheme-indent-function 1))
+  (eval . (put 'call-with-inferior 'scheme-indent-function 1))
   (eval . (put 'maybe-let* 'scheme-indent-function 1))
   (eval . (put 'maybe-assoc-set 'scheme-indent-function 1))))
