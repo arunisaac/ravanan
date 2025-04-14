d2329d6da234edf2becbf862a9f63c4e067596f9
Arun Isaac <arunisaac@systemreboot.net>
2025-04-14 16:26:44 +0100

    dir-locals: Indent call-with-atomic-output-file correctly.
    * .dir-locals.el (scheme-mode): Indent call-with-atomic-output-file
    correctly.

--- .dir-locals.el
+++ .dir-locals.el
@@ -9,6 +9,7 @@
   (fill-column . 80)
   (eval . (put 'assoc-set 'scheme-indent-function 1))
   (eval . (put 'build-gexp-script 'scheme-indent-function 1))
+  (eval . (put 'call-with-atomic-output-file 'scheme-indent-function 1))
   (eval . (put 'call-with-current-directory 'scheme-indent-function 1))
   (eval . (put 'call-with-inferior 'scheme-indent-function 1))
   (eval . (put 'maybe-let* 'scheme-indent-function 1))
