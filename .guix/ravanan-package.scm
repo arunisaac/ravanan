9c885abfc358a6255a2fe5435d88467721ae8f8e
Arun Isaac <arunisaac@systemreboot.net>
2025-11-17 18:35:52 +0000

    guix: Build run64 without guile-chibi-match.

--- .guix/ravanan-package.scm
+++ .guix/ravanan-package.scm
@@ -18,59 +18,16 @@
 
 (define-module (ravanan-package)
   #:use-module ((gnu packages bioinformatics) #:select (ravanan) #:prefix guix:)
-  #:use-module ((gnu packages guile) #:select (guile-3.0))
-  #:use-module ((gnu packages scheme) #:select (chibi-scheme))
   #:use-module (guix build-system gnu)
-  #:use-module (guix build-system guile)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix utils))
 
-(define-public guile-chibi-match
-  (package
-    (name "guile-chibi-match")
-    (version (package-version chibi-scheme))
-    (source (package-source chibi-scheme))
-    (build-system guile-build-system)
-    (arguments
-     (list #:modules '((guix build guile-build-system)
-                       (guix build utils)
-                       (ice-9 ftw))
-           #:phases
-           #~(modify-phases %standard-phases
-               (add-after 'unpack 'rearrange-files
-                 (lambda _
-                   ;; Delete and rearrange files so that (chibi match)
-                   ;; is in the expected directory hierarchy.
-                   (chdir "lib/chibi")
-                   (rename-file "match" "chibi")
-                   (for-each (lambda (file)
-                               (unless (member file (list "." ".." "chibi"))
-                                 (delete-file-recursively file)))
-                             (scandir "."))
-                   (substitute* "chibi/match.scm"
-                     ((";; 2006/12/01[^\n]*" line)
-                      (string-append line
-                                     "\n(define-module (chibi match)
-#:export (match match-lambda match-lambda* match-let match-letrec match-let*))"))))))))
-    (inputs
-     (list guile-3.0))
-    (home-page "https://synthcode.com/scheme/chibi/lib/chibi/match.html")
-    (synopsis "Portable hygienic pattern matcher")
-    (description "@code{guile-chibi-match} is a portable hygienic
-pattern matcher for Scheme.  This is a full superset of the popular
-match package by Andrew Wright, written in fully portable syntax-rules
-and thus preserving hygiene.  The most notable extensions are the
-ability to use non-linear patterns---patterns in which the same
-identifier occurs multiple times, tail patterns after ellipsis, and
-the experimental tree patterns.")
-    (license (package-license chibi-scheme))))
-
-(define-public run64
-  (let ((commit "79976781fd609409cd5ac543b3ecfa4d2531dd6a")
-        (revision "0"))
+(define-public guile-run64
+  (let ((commit "5b2bc2d146e7b58f15e1bc653291b71d8376b763")
+        (revision "1"))
     (package
       (name "run64")
       (version (git-version "0.1.0" revision commit))
@@ -82,33 +39,13 @@ the experimental tree patterns.")
                 (file-name (git-file-name name version))
                 (sha256
                  (base32
-                  "1fxz5463hv609hqmqn3rph3z9fd4viqxfxxx2js1y75zqqyb1ab9"))))
+                  "0wim4vxqzvwdvmdgx5q7ws03qqj392dh8hcph4yxxkqijnabqgvw"))))
       (build-system gnu-build-system)
       (arguments
        (list #:make-flags #~(list (string-append "prefix=" #$output))
-             #:modules `(((guix build guile-build-system)
-                          #:select (target-guile-effective-version))
-                         ,@%default-gnu-imported-modules)
              #:phases
-             (with-imported-modules `((guix build guile-build-system)
-                                      ,@%default-gnu-imported-modules)
-               #~(modify-phases %standard-phases
-                   (delete 'configure)
-                   (add-after 'install 'wrap
-                     (lambda* (#:key inputs outputs #:allow-other-keys)
-                       (let ((out (assoc-ref outputs "out"))
-                             (effective-version (target-guile-effective-version)))
-                         (wrap-program (string-append out "/bin/guile-run64")
-                           `("GUILE_LOAD_PATH" prefix
-                             (,(string-append out "/share/guile/site/" effective-version)
-                              ,(getenv "GUILE_LOAD_PATH")))
-                           `("GUILE_LOAD_COMPILED_PATH" prefix
-                             (,(string-append out "/lib/guile/"
-                                              effective-version "/site-ccache")
-                              ,(getenv "GUILE_LOAD_COMPILED_PATH")))))))))))
-      (inputs
-       (list guile-3.0
-             guile-chibi-match))
+             #~(modify-phases %standard-phases
+                 (delete 'configure))))
       (home-page "https://run64.systemreboot.net")
       (synopsis "SRFI-64 test runner for Scheme")
       (description "run64 is a SRFI-64 test runner for Scheme.")
@@ -134,6 +71,6 @@ the experimental tree patterns.")
                                           file stat)))))
     (native-inputs
      (modify-inputs (package-native-inputs guix:ravanan)
-       (prepend run64)))))
+       (prepend guile-run64)))))
 
 ravanan
